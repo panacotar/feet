8ca7134b65e4cabddc758dfb3bbc8225ba87c18e
Darius Pirvulescu <organicdarius@gmail.com>
2022-10-10 22:48:01 +0100

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## [Unreleased]
+
+## [0.0.1] - 2022-10-10
+
+- Initial release
---
+++ lib/feet.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "feet/version"
+
+module Feet
+  class Error < StandardError; end
+  # Your code goes here...
+end
