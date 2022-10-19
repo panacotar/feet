1db511ee704f4f6a9c7a42a430857fb2e6d08b8b
Darius Pirvulescu <organicdarius@gmail.com>
2022-10-20 00:07:45 +0100

    Add default index.html response

--- lib/feet.rb
+++ lib/feet.rb
@@ -13,6 +13,10 @@ module Feet
       if env['PATH_INFO'] == '/favicon.ico'
         return [404, {'Content-Type' => 'text/html'}, []]
       end
+      if env["PATH_INFO"] == "/default"
+        path = File.expand_path('../public/index.html', __FILE__)
+        return [200, {"Content-Type" => "text/html"}, File.open(path)]
+      end
       klass, action = get_controller_and_action(env)
       controller = klass.new(env)
       if post?(env)
