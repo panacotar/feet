5e43a96039186048cccf2e55415f3744b91f8214
Darius Pirvulescu <organicdarius@gmail.com>
2022-10-19 23:02:57 +0100

    Check if the request of of type POST

--- lib/feet.rb
+++ lib/feet.rb
@@ -10,9 +10,15 @@ module Feet
   class Application
     def call(env)
 
+      if env['PATH_INFO'] == '/favicon.ico'
+        return [404, {'Content-Type' => 'text/html'}, []]
+      end
       klass, action = get_controller_and_action(env)
       controller = klass.new(env)
-      text = controller.send(action)
+      if post?(env)
+        `echo "a new POST #{env['PATH_INFO']}" > debug.txt`;
+      end
+
 
       [200, {'Content-Type' => 'text/html'},
         [text]]
