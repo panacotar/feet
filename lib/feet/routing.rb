9192c9e99f9a479f85eff67f2624d06a87ba72b4
Darius Pirvulescu <organicdarius@gmail.com>
2022-10-19 23:37:37 +0100

    Change code for the default action

--- lib/feet/routing.rb
+++ lib/feet/routing.rb
@@ -2,7 +2,7 @@ module Feet
   class Application
     def get_controller_and_action(env)
       `echo #{env["PATH_INFO"]} >> debug.txt`
-      action = action.nil? ? 'index' : action
+      action ||= 'index'
       cont += "Controller"
 
       [Object.const_get(cont), action]
