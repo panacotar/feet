1979e930f9a4b3ef3de213769706332ceb51e071
Darius Pirvulescu <organicdarius@gmail.com>
2022-10-23 20:35:28 +0100

    Add a redirect path to Application

--- lib/feet.rb
+++ lib/feet.rb
@@ -13,12 +13,20 @@ module Feet
       if env['PATH_INFO'] == '/favicon.ico'
         return [404, {'Content-Type' => 'text/html'}, []]
       end
+
       if env["PATH_INFO"] == "/default"
         path = File.expand_path('../public/index.html', __dir__)
         return [200, {"Content-Type" => "text/html"}, File.open(path)]
       end
+
+      if env["PATH_INFO"] == "/redirect"
+        # Perform a redirect
+        return [301, {'Location' => '/quotes/a_quote'}, []]
+      end
+
       klass, action = get_controller_and_action(env)
       controller = klass.new(env)
+
       if post?(env)
         `echo "a new POST #{env['PATH_INFO']}" > debug.txt`;
       end
