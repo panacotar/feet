fd9b1f771bba05b7391efdb326d1e883741f102e
Darius Pirvulescu <organicdarius@gmail.com>
2022-10-24 22:22:52 +0100

    Fix code in dependencies.rb

--- lib/feet/dependencies.rb
+++ lib/feet/dependencies.rb
@@ -2,7 +2,7 @@ class Object
   def self.const_missing(c)
     require_relative '../utils/namespace.rb'
     namesp = Namespace.new
-    formatted_c = namesp.to_snake_case(c.to_s)
-    Object.const_get(formatted_c)
+    require namesp.to_snake_case(c.to_s)
+    Object.const_get(c)
   end
 end
