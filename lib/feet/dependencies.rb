66bf1ffa65494b770fe09f50fb1701004b0afe77
Darius Pirvulescu <organicdarius@gmail.com>
2022-10-25 21:35:32 +0100

    Prevent running const_missing for different controller

--- lib/feet/dependencies.rb
+++ lib/feet/dependencies.rb
@@ -1,16 +1,16 @@
 class Object
   def self.const_missing(c)
-    return nil if @const_missing_called
+    return nil if @const_missing_called == c.to_s
 
     require_relative '../utils/namespace.rb'
     namesp = Namespace.new
 
     `echo #{namesp.to_snake_case(c.to_s)} >> fail.txt`
     `echo called #{@const_missing_called.to_s} >> fail.txt`
-    @const_missing_called = true
+    @const_missing_called = c.to_s
     require namesp.to_snake_case(c.to_s)
     klass = Object.const_get(c)
-    @const_missing_called = false
+    @const_missing_called = nil
 
     klass
   end
