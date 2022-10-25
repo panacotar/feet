d6894db97e6aade1ad374d2df07adb8b818dd112
Darius Pirvulescu <organicdarius@gmail.com>
2022-10-25 21:06:54 +0100

    Test infinite loop for const_missing

--- lib/feet/dependencies.rb
+++ lib/feet/dependencies.rb
@@ -2,6 +2,7 @@ class Object
   def self.const_missing(c)
     require_relative '../utils/namespace.rb'
     namesp = Namespace.new
+    `echo #{namesp.to_snake_case(c.to_s)} >> fail.txt`
     require namesp.to_snake_case(c.to_s)
     Object.const_get(c)
   end
