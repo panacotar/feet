361ee38f26585436e742a751f60913b243de6202
Darius Pirvulescu <organicdarius@gmail.com>
2022-10-25 21:18:45 +0100

    Fix infinite loop in dependencies.rb
    this can be if the controller has a differnt name than its file, ex: FooController in a test_controller.rb file instead of a foo_controller.rb

--- lib/feet/dependencies.rb
+++ lib/feet/dependencies.rb
@@ -1,9 +1,17 @@
 class Object
   def self.const_missing(c)
+    return nil if @const_missing_called
+
     require_relative '../utils/namespace.rb'
     namesp = Namespace.new
+
     `echo #{namesp.to_snake_case(c.to_s)} >> fail.txt`
+    `echo called #{@const_missing_called.to_s} >> fail.txt`
+    @const_missing_called = true
     require namesp.to_snake_case(c.to_s)
-    Object.const_get(c)
+    klass = Object.const_get(c)
+    @const_missing_called = false
+
+    klass
   end
 end
