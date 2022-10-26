5a93c021b2693ee1c8147bc18d8dff28112ead54
Darius Pirvulescu <organicdarius@gmail.com>
2022-10-26 09:08:00 +0100

    Experiment with to_snake_case method

--- public/exp/const_missing.rb
+++ public/exp/const_missing.rb
@@ -7,3 +7,16 @@ class Object
 end
 
 Bobo.new.print_bobo
+
+def to_snake_case(s)
+  # p s.gsub(/([a-z\d])([A-Z])/,'\1_\2')
+  p s.gsub(/::/, '/')
+     .gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
+     .gsub(/([a-z\d])([A-Z][a-z])/, '\1_\2')
+     .tr('-', '_')
+     .downcase
+end
+
+a = ["CAMELCase", "CamelCase", "ExtremeCamelCase", "Camel2Case", "Not-Friendly", "Name::SPACE"]
+
+a.each { |s| to_snake_case(s) }
