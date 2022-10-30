92ed53edd9b640f4390b941377ff73622c939ad0
Darius Pirvulescu <organicdarius@gmail.com>
2022-10-30 23:41:53 +0000

    Add render method to controller

--- lib/feet/controller.rb
+++ lib/feet/controller.rb
@@ -1,3 +1,5 @@
+require 'erubis'
+
 module Feet
   class Controller
     def initialize(env)
@@ -12,5 +14,11 @@ module Feet
       self.class.to_s.split('Controller').first.downcase
     end
 
+    def render(view_name, locals = {})
+      filename = File.join 'app', 'views', me.to_s, "#{view_name}.html.erb"
+      template = File.read filename
+      eruby = Erubis::Eruby.new(template)
+      eruby.result locals.merge(:env => env)
+    end
   end
 end
