f7bf573da6afbfdc7f75305329393580474f96ae
Darius Pirvulescu <organicdarius@gmail.com>
2022-11-27 12:03:16 +0000

    Modify controller_name method

--- lib/feet/controller.rb
+++ lib/feet/controller.rb
@@ -10,12 +10,15 @@ module Feet
       @env
     end
 
-    def me
-      self.class.to_s.split('Controller').first.downcase
+    def controller_name
+      # self.class.to_s.split('Controller').first.downcase
+      klass = self.class
+      klass = klass.to_s.gsub(/Controller$/, '')
+      Feet.to_snake_case klass
     end
 
     def render(view_name, locals = {})
-      filename = File.join 'app', 'views', me.to_s, "#{view_name}.html.erb"
+      filename = File.join 'app', 'views', controller_name, "#{view_name}.html.erb"
       template = File.read filename
       eruby = Erubis::Eruby.new(template)
       eruby.result locals.merge(:env => env)
