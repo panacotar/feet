2796e076b32dac7aa062a7e19e6d75b152dc6b24
Darius Pirvulescu <organicdarius@gmail.com>
2022-12-01 14:47:46 +0000

    Require FileModel on the file_name playground

--- public/exp/file_name.rb
+++ public/exp/file_name.rb
@@ -1,4 +1,6 @@
-name = 'db/quotes/3.json'
+require_relative '../../lib/feet/file_model.rb'
+
+name = 'public/exp/db/quotes/3.json'
 
 basename = File.split(name)[-1]
 
@@ -7,3 +9,7 @@ p basename
 id = File.basename(basename, '.json').to_i
 
 p id
+
+file = Feet::Model::FileModel.new(name)
+
+p file.send(:[], 'Marco')
