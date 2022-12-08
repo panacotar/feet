56e2bf9fc9ce58f4e2ca5b290e662333b9432974
Darius Pirvulescu <organicdarius@gmail.com>
2022-12-08 17:49:34 +0000

    Create file_init in file model test

--- test/file_model_test.rb
+++ test/file_model_test.rb
@@ -11,20 +11,24 @@ class FeetTestUtils < Minitest::Test
     File.join(base, path)
   end
 
+  def file_init
+    Feet::Model::FileModel.new(entry)
+  end
+
   def test_init
-    file = Feet::Model::FileModel.new(entry)
+    file = file_init
 
     assert file.instance_of?(Feet::Model::FileModel)
   end
 
   def test_reader
-    file = Feet::Model::FileModel.new(entry)
+    file = file_init
 
     assert file['Marco'] == 'Polo'
   end
 
   def test_setter
-    file = Feet::Model::FileModel.new(entry)
+    file = file_init
 
     file['Andrew'] = 'Polo'
 
