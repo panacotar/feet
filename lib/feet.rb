c01311aaa778de9b6d74955d86c6e2ecf2a7e698
Darius Pirvulescu <organicdarius@gmail.com>
2022-12-10 22:02:52 +0000

    Fix linter warnings in Application

--- lib/feet.rb
+++ lib/feet.rb
@@ -12,26 +12,22 @@ module Feet
 
   class Application
     def call(env)
-      if env['PATH_INFO'] == '/favicon.ico'
-        return [404, {'Content-Type' => 'text/html'}, []]
-      end
+      return [404, { 'Content-Type' => 'text/html' }, []] if env['PATH_INFO'] == '/favicon.ico'
 
-      if env["PATH_INFO"] == "/default"
+      if env['PATH_INFO'] == '/default'
         path = File.expand_path('../public/index.html', __dir__)
-        return [200, {"Content-Type" => "text/html"}, File.open(path)]
+        return [200, { 'Content-Type' => 'text/html' }, File.open(path)]
       end
 
-      if env["PATH_INFO"] == "/redirect"
+      if env['PATH_INFO'] == '/redirect'
         # Perform a redirect
-        return [301, {'Location' => '/quotes/a_quote'}, []]
+        return [301, { 'Location' => '/quotes/a_quote' }, []]
       end
 
       klass, action = get_controller_and_action(env)
       controller = klass.new(env)
 
-      if post?(env)
-        `echo "a new POST #{env['PATH_INFO']}" > debug.txt`
-      end
+      `echo "a new POST #{env['PATH_INFO']}" > debug.txt` if post?(env)
 
       begin
         text = controller.send(action)
