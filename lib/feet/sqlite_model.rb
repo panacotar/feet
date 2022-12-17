feb893ee3528cc17d1ee6c4dd715f96adca68b79
Darius Pirvulescu <organicdarius@gmail.com>
2022-12-17 13:53:48 +0000

    Add initialize and count class method to SQLiteModel

--- lib/feet/sqlite_model.rb
+++ lib/feet/sqlite_model.rb
@@ -6,6 +6,10 @@ DB = SQLite3::Database.new 'test.db'
 module Feet
   module Model
     class SQLiteModel
+      def self.initialize(data = nil)
+        @hash = data
+      end
+
       def self.table
         Feet.to_snake_case name # name = method to return the class name, ex: MyName
       end
@@ -19,6 +23,13 @@ module Feet
         end
         @schema
       end
+
+      def self.count
+        DB.execute <<~SQL
+          SELECT COUNT(*) FROM #{table}
+        SQL
+      end
+
     end
   end
 end
