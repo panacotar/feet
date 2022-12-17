313e4dfe7377e3c1c02f4c087a1a871333cd1a3d
Darius Pirvulescu <organicdarius@gmail.com>
2022-12-17 15:19:11 +0000

    Add more to sqlite exp playground

--- public/exp/sqlite_play.rb
+++ public/exp/sqlite_play.rb
@@ -11,3 +11,12 @@ p MyTable.table
 puts '>>> count'
 p MyTable.count
 
+puts '>>> create'
+item = {"title"=>"Sensational", "id"=>2121, "posted"=>1, "body"=>"whatevs"}
+result = MyTable.create(item)
+p result
+
+# puts '>>> new instance'
+# it = {"posted"=>1, "title"=>"my title", "body"=>"whatevs", "id"=>7}
+# a = MyTable.new(it)
+# p a
