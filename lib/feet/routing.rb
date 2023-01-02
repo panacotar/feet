0a0c7b9cb9ed1c7218af039fba97a6c987cbbac3
Darius Pirvulescu <organicdarius@gmail.com>
2023-01-02 14:53:48 +0000

    Create routing -> RouteObject with match method

--- lib/feet/routing.rb
+++ lib/feet/routing.rb
@@ -1,4 +1,57 @@
 module Feet
+  class RouteObject
+    def initialize
+      @rules = []
+    end
+
+    # Example arguments
+    # url  ":controller/:id"
+    # args [{:default=>{"action"=>"show"}}]
+    def match(url, *args)
+      # Capture the options hash
+      options = {}
+      options = args.pop if args[-1].is_a? Hash
+      # Check for default option
+      options[:default] ||= {}
+
+      # Get destination and limit the # of arguments
+      dest = nil
+      dest = args.pop if args.size > 0
+      raise 'Too many arguments!' if args.size > 0
+
+      # Parse URL parts
+      parts = url.split('/')
+      # parts.select! { |p| !p.empty? }
+      parts.reject! { |p| p.empty? }
+
+      vars = []
+      regexp_parts = parts.map do |part|
+        case part[0]
+        when ':'
+          # Map Variable
+          vars << part[1..-1]
+          '([a-zA-Z0-9]+)'
+        when '*'
+          # Map Wildcard
+          vars << part[1..-1]
+          '(.*)'
+        else
+          part
+        end
+      end
+
+      regexp = regexp_parts.join('/')
+      @rules.push({
+                    regexp: Regexp.new("^/#{regexp}$"),
+                    vars: vars,
+                    dest: dest,
+                    options: options
+                  })
+    end
+
+
+  end
+
   class Application
     def get_controller_and_action(env)
       _, cont, action, _after = env["PATH_INFO"].split('/', 4)
